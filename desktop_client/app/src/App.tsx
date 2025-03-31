1fc163367a933de927e935967d4df16b83baa48f
Danny Piper <djpiper28@gmail.com>
2025-03-31 18:48:21 +0100

    feat: started work on theming shadcn

--- desktop_client/app/src/App.tsx
+++ desktop_client/app/src/App.tsx
@@ -1,12 +1,33 @@
-import { H1 } from "@/components/typography/H1";
-import { Menubar } from "@/components/ui/menubar";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "@radix-ui/react-navigation-menu";
+import { H1 } from "./components/typography/H1";
 
 function App() {
   return (
     <div className="flex flex-col gap-3">
-      <Menubar id="menu">
-        <H1>RPG Book</H1>
-      </Menubar>
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <H1>RPG Book</H1>
+            <NavigationMenuTrigger>Projects</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <NavigationMenuLink>Create a project</NavigationMenuLink>
+            </NavigationMenuContent>
+            <NavigationMenuTrigger>Settings</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <NavigationMenuLink>Appearance</NavigationMenuLink>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+
+      <p className="text-red-500">TEST</p>
     </div>
   );
 }
