be9fc8a651d3a2965176eb0a84f7d4fb2843a907
Danny Piper <djpiper28@gmail.com>
2025-04-07 22:37:16 +0100

    feat: added logs to generate files

--- desktop_client/app/generate-shadcn.go
+++ desktop_client/app/generate-shadcn.go
@@ -1,3 +1,5 @@
 package app
 
+//go:generate echo Generating shadcn... 
+
 //go:generate pnpm dlx shadcn@latest add -y -o -a
--- generate-tygo.go
+++ generate-tygo.go
@@ -1,3 +1,5 @@
 package rpgbook
 
+//go:generate echo Generating tygo types...
+
 //go:generate tygo generate
