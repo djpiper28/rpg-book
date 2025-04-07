bc4ae72772ca68fd1be258a56bc52300eff38449
Danny Piper <djpiper28@gmail.com>
2025-04-07 22:39:43 +0100

    feat: added more logs to generate

--- desktop_client/backend/pb_common/generate.go
+++ desktop_client/backend/pb_common/generate.go
@@ -1,3 +1,5 @@
 package pb_common
 
+//go:generate echo Generating common protos...
+
 //go:generate protoc --proto_path=../../protos/ --go_out=. --go_opt=paths=source_relative ../../protos/common.proto
--- desktop_client/backend/pb_system/generate.go
+++ desktop_client/backend/pb_system/generate.go
@@ -1,3 +1,5 @@
 package pb_system
 
+//go:generate echo Generating system protos...
+
 //go:generate protoc --proto_path=../../protos/ --go_out=. --go_opt=paths=source_relative ../../protos/system.proto --go-grpc_out=. --go-grpc_opt=paths=source_relative
