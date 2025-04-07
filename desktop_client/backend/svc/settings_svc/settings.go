3a8a6dfb72a3748181446c07b57790d812bd5cc8
Danny Piper <djpiper28@gmail.com>
2025-04-07 16:07:15 +0100

    fix: send empty pb

--- desktop_client/backend/svc/settings_svc/settings.go
+++ desktop_client/backend/svc/settings_svc/settings.go
@@ -2,7 +2,6 @@ package settingssvc
 
 import (
 	"context"
-	"errors"
 
 	"github.com/djpiper28/rpg-book/desktop_client/backend/pb_common"
 	"github.com/djpiper28/rpg-book/desktop_client/backend/pb_settings"
@@ -17,5 +16,5 @@ func New() *SettingsSvc {
 }
 
 func (s *SettingsSvc) GetSettings(ctx context.Context, in *pb_common.Empty) (*pb_settings.Settings, error) {
-	return nil, errors.New("Not implemented")
+	return &pb_settings.Settings{}, nil
 }
