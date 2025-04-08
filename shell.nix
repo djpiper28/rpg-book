0b74fda0b60cfa34a05cb067385ee8a8706a0deb
Danny Piper <djpiper28@gmail.com>
2025-04-08 17:31:54 +0100

    feat: added missing deps

--- shell.nix
+++ shell.nix
@@ -5,10 +5,14 @@ pkgs.mkShell {
     curl
     git
     nodejs_22
+    pnpm
     go
     golangci-lint
     electron_35
     p7zip
+    protobuf_27
     protoc-gen-js
+    protoc-gen-grpc-web
+    grpc-tools
   ];
 }
