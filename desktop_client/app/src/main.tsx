28f571013d692f5373beef4222847b5055ab2f68
Danny Piper <danny.piper@motorolasolutions.com>
2025-04-14 16:32:20 +0100

    fix: mantine CSS

--- desktop_client/app/src/main.tsx
+++ desktop_client/app/src/main.tsx
@@ -2,11 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { MantineProvider } from "@mantine/core";
+import '@mantine/core/styles.css';
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <MantineProvider defaultColorScheme="dark">
+    <MantineProvider
+      withCSSVariables
+      withGlobalStyles
+      withNormalizeCSS
+      defaultColorScheme="dark"
+    >
       <App />
     </MantineProvider>
   </React.StrictMode>,
