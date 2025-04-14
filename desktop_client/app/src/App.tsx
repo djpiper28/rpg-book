896ad56851f40f10c62bc5a976855043c37b1d5e
Danny Piper <djpiper28@gmail.com>
2025-04-14 22:32:00 +0100

    feat: added padding to app

--- desktop_client/app/src/App.tsx
+++ desktop_client/app/src/App.tsx
@@ -2,17 +2,20 @@ import { H1 } from "@/components/typography/H1";
 import { Settings } from "lucide-react";
 import { logger } from "./lib/grpcClient/client";
 import { Button } from "@mantine/core";
+import { P } from "./components/typography/P";
 
 function App() {
   logger.warn("render call", {});
   return (
-    <div className="flex flex-col gap-3">
+    <div className="flex flex-col gap-3 p-2">
       <div id="menu" className="flex flex-row gap-3 justify-between">
         <H1>RPG Book</H1>
-        <Button aria-label="Open settings" variant>
+        <Button aria-label="Open settings" variant="filled">
           <Settings /> Settings
         </Button>
       </div>
+
+      <P>Lorem ipsum sit amet dolor</P>
     </div>
   );
 }
