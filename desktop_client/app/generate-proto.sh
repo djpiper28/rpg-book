8738b026dcee58b462e84bbeb1fc534ba8d34ea3
Danny Piper <djpiper28@gmail.com>
2025-08-06 20:06:24 +0100

    feat: use common args for protoc generation

--- desktop_client/app/generate-proto.sh
+++ desktop_client/app/generate-proto.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-set -e
+args="--ts_out ./src/lib/grpcClient/pb/ --proto_path=../protos/"
+proto_files="../protos/*.proto"
 
 if [[ "$(uname)" == "Darwin" ]] || [[ "$(uname)" == "Linux" ]]; then
-  bash -c "cd ./src/lib/grpcClient/pb/ && bash clean.sh"
-  bash -c "protoc --ts_out ./src/lib/grpcClient/pb/ --plugin=protoc-gen-ts=./node_modules/.bin/protoc-gen-ts --proto_path=../protos/ ../protos/*.proto"
+  protoc $args --plugin=protoc-gen-ts=./node_modules/.bin/protoc-gen-ts $proto_files || exit 1
 else
   # We are on windows
-  bash -c "cd ./src/lib/grpcClient/pb/ && bash clean.sh"
-  bash -c "protoc --ts_out ./src/lib/grpcClient/pb/ --plugin=protoc-gen-ts=./node_modules/.bin/protoc-gen-ts.cmd --proto_path=../protos/ ../protos/*.proto"
+  protoc $args --plugin=protoc-gen-ts=./node_modules/.bin/protoc-gen-ts.cmd $proto_files || exit 1
 fi
