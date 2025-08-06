23b2062f0d91a315356ede2985276c3af1f0b0fe
Danny Piper <djpiper28@gmail.com>
2025-08-06 20:44:37 +0100

    fix: may work on windows

--- desktop_client/app/generate-proto.sh
+++ desktop_client/app/generate-proto.sh
@@ -6,6 +6,7 @@ proto_files="../protos/*.proto"
 if [[ "$(uname)" == "Darwin" ]] || [[ "$(uname)" == "Linux" ]]; then
   protoc $args --plugin=protoc-gen-ts=./node_modules/.bin/protoc-gen-ts $proto_files || exit 1
 else
+  protoc $args --plugin=protoc-gen-ts=./node_modules/.bin/protoc-gen-ts $proto_files || exit 1
   # We are on windows
-  protoc $args --plugin=protoc-gen-ts=./node_modules/.bin/protoc-gen-ts.cmd $proto_files || exit 1
+  # protoc $args --plugin=protoc-gen-ts=./node_modules/.bin/protoc-gen-ts.cmd $proto_files || exit 1
 fi
