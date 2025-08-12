c41feabfe7cb42e5c6d83b62f69e2a76b4712719
Danny Piper <djpiper28@gmail.com>
2025-08-12 21:06:20 +0100

    feat: added edit button for characters

--- desktop_client/app/src/pages/project/tabs/characterTab.tsx
+++ desktop_client/app/src/pages/project/tabs/characterTab.tsx
@@ -4,8 +4,12 @@ import { Modal } from "@/components/modal/modal";
 import { useProjectStore } from "@/stores/projectStore";
 import { useTabStore } from "@/stores/tabStore";
 import CreateCharacterModal from "./createCharacterModal";
+import { useState } from "react";
+import { P } from "@/components/typography/P";
+import { Pencil } from "lucide-react";
 
 export function CharacterTab() {
+  const [selectedCharacter, setSelectedCharacter] = useState("");
   const [opened, { close, open }] = useDisclosure(false);
   const projectHandle = useTabStore((x) => x.selectedTab);
   const projectStore = useProjectStore((x) => x);
@@ -35,12 +39,33 @@ export function CharacterTab() {
             <Table.Th>Name</Table.Th>
             <Table.Th>Factions</Table.Th>
           </Table.Tr>
-          {thisProject.project.characters.map((character) => (
-            <Table.Tr key={character.handle?.id}>
-              <Table.Th>{character.name}</Table.Th>
-              <Table.Th>TODO: Change me</Table.Th>
-            </Table.Tr>
-          ))}
+          {thisProject.project.characters.map((character) => {
+            const id = character.handle?.id ?? "";
+            return (
+              <Table.Tr
+                className={selectedCharacter == id ? "bg-gray-600" : ""}
+                key={id}
+                onClick={() => {
+                  setSelectedCharacter(id);
+                }}
+              >
+                <Table.Th>
+                  <div className="flex flex-row gap-2">
+                    <button
+                      className="cursor-pointer"
+                      onClick={() => {
+                        // TODO: open view / update dialog
+                      }}
+                    >
+                      <Pencil className="p-1" />
+                    </button>
+                    <P className="text-wrap">{character.name}</P>
+                  </div>
+                </Table.Th>
+                <Table.Th>TODO: Change me</Table.Th>
+              </Table.Tr>
+            );
+          })}
         </Table.Thead>
       </Table>
     </>
