aa13264ff6b39295c69d8f36ea321f4fa5dddf18
Danny Piper <djpiper28@gmail.com>
2025-09-05 16:06:37 +0100

    chore: comment out unused tabs

--- desktop_client/app/src/pages/project/project.tsx
+++ desktop_client/app/src/pages/project/project.tsx
@@ -22,12 +22,14 @@ export function Component() {
       <Tabs defaultValue="characters">
         <Tabs.List>
           <H2>{currentTab.name}</H2>
-          <Tabs.Tab value="notes">Notes</Tabs.Tab>
           <Tabs.Tab value="characters">Characters</Tabs.Tab>
+          {/*
+          <Tabs.Tab value="notes">Notes</Tabs.Tab>
           <Tabs.Tab value="items">Items</Tabs.Tab>
           <Tabs.Tab value="factions">Factions</Tabs.Tab>
           <Tabs.Tab value="maps">Maps &amp; Places</Tabs.Tab>
           <Tabs.Tab value="settings">Settings</Tabs.Tab>
+          */}
         </Tabs.List>
 
         <Tabs.Panel value="characters">
