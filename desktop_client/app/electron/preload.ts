26e32721a5c7c44d9939e60a8c01b0871f6bfe96
Danny Piper <djpiper28@gmail.com>
2025-09-15 18:03:54 +0100

    fix: import of fs() in preload

--- desktop_client/app/electron/preload.ts
+++ desktop_client/app/electron/preload.ts
@@ -1,13 +1,28 @@
 // eslint-disable-next-line import-x/no-unresolved
+import { dialog, shell } from "@electron/remote";
 import { contextBridge } from "electron";
-import {
-  electronDialog,
-  electronShell,
-  getBuildType,
-  getEnv,
-  getSystemVersion,
-  read,
-} from "../src/lib/electron/main";
+import { promises } from "node:fs";
+
+const electronShell = shell;
+const electronDialog = dialog;
+
+function getSystemVersion(): string {
+  return process.getSystemVersion();
+}
+
+function getEnv(): Record<string, string> {
+  return {
+    RPG_BOOK_PORT: process.env.RPG_BOOK_PORT,
+  };
+}
+
+function getBuildType(): string {
+  return process.env.VITE_DEV_SERVER_URL ? "Development" : "Production";
+}
+
+async function read(url: string): Promise<Uint8Array> {
+  return await promises.readFile(url);
+}
 
 contextBridge.exposeInMainWorld("electron", {
   dialog: electronDialog,
