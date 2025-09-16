5167fd49dad47feb1aa8c354d436fcbd35619e69
Danny Piper <danny.piper@motorolasolutions.com>
2025-09-16 10:28:12 +0100

    feat: started work on single character edit model

---
+++ desktop_client/app/src/pages/project/tabs/characterEdit.tsx
@@ -0,0 +1,49 @@
+import { TextInput } from "@mantine/core";
+import { type ReactNode } from "react";
+import { IconSelector } from "@/components/input/iconSelector";
+import { MarkdownEditor } from "@/components/input/markdownEditor";
+import { type BasicCharacterDetails } from "@/lib/grpcClient/pb/project_character";
+
+interface Props {
+  character: BasicCharacterDetails;
+  // base 64 or a file:// URL
+  icon: string;
+  setCharacter: (details: BasicCharacterDetails) => void;
+  setIcon: (icon: string) => void;
+}
+
+export function CharacterEdit(props: Readonly<Props>): ReactNode {
+  return (
+    <div className="flex flex-col gap-3">
+      <div className="flex flex-row gap-3 justify-between items-start">
+        <TextInput
+          label="Character Name"
+          onChange={(x) => {
+            const details = structuredClone(props.character);
+            details.name = x.target.value;
+            props.setCharacter(details);
+          }}
+          placeholder="John Smith"
+          required={true}
+          value={props.character.name}
+        />
+        <IconSelector
+          description="Select an icon for your character"
+          filepath={props.icon}
+          setFilepath={(src) => {
+            props.setIcon(src);
+          }}
+        />
+      </div>
+      <MarkdownEditor
+        label="Description and notes"
+        setValue={(value) => {
+          const details = structuredClone(props.character);
+          details.description = value;
+          props.setCharacter(details);
+        }}
+        value={props.character.description}
+      />
+    </div>
+  );
+}
