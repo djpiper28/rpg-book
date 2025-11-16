60b285eb880f013b1da53f3e2489bb4347dd7779
Danny Piper <djpiper28@gmail.com>
2025-11-16 14:41:46 +0000

    feat: added working basic query

--- common/search/parser/simple_test.go
+++ common/search/parser/simple_test.go
@@ -1,8 +1,35 @@
 package parser_test
 
+// This test only makes sure that things can be parsed. The output is not checked!
+
 import (
+	"go/parser"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestBasicQuery(t *testing.T) {
+	_, err := parser.ParseExpr("John123")
+	require.NoError(t, err)
+}
+
+func TestQueryThenQuery(t *testing.T) {
+	_, err := parser.ParseExpr("John Smith")
+	require.NoError(t, err)
+}
+
+func TestAndOperator(t *testing.T) {
+	_, err := parser.ParseExpr("John and Smith")
+	require.NoError(t, err)
+}
+
+func TestOrOperator(t *testing.T) {
+	_, err := parser.ParseExpr("John or Smith")
+	require.NoError(t, err)
+}
+
+func TestXorOperator(t *testing.T) {
+	_, err := parser.ParseExpr("John xor Smith")
+	require.NoError(t, err)
 }
