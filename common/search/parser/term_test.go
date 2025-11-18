84e34c4591edbdf10d249adc9426156c67622340
Danny Piper <danny.piper@motorolasolutions.com>
2025-11-18 10:12:20 +0000

    fix: >= and > are now assigned correctly

--- common/search/parser/term_test.go
+++ common/search/parser/term_test.go
@@ -8,7 +8,7 @@ import (
 )
 
 func TestBasicQuery(t *testing.T) {
-  const testString = "Testing-123"
+	const testString = "Testing-123µ"
 	ast, err := parser.Parse(testString)
 	require.NoError(t, err)
 	require.Equal(t, parser.NodeType_Basic, ast.Type)
@@ -21,4 +21,24 @@ func TestBasicQueryQuotation(t *testing.T) {
 	require.Equal(t, parser.NodeType_Basic, ast.Type)
 	require.Equal(t, `testing 123
 test`, ast.BasicQuery.Value)
+}
+
+func TestMixedSetGenerator(t *testing.T) {
+	ast, err := parser.Parse(`"test":value-123`)
+	require.NoError(t, err)
+	require.Equal(t, parser.NodeType_SetGenerator, ast.Type)
+	require.Equal(t, "test", ast.SetGenerator.Key)
+	require.Equal(t, "value-123", ast.SetGenerator.Value)
+	require.False(t, ast.SetGenerator.Negated)
+	require.Equal(t, parser.GeneratorOperator_Includes, ast.SetGenerator.GeneratorOperator)
+}
+
+func TestNegatedMixedSetGenerator(t *testing.T) {
+	ast, err := parser.Parse(`-power>="5.3"`)
+	require.NoError(t, err)
+	require.Equal(t, parser.NodeType_SetGenerator, ast.Type)
+	require.Equal(t, "power", ast.SetGenerator.Key)
+	require.Equal(t, "5.3", ast.SetGenerator.Value)
+	require.True(t, ast.SetGenerator.Negated)
+	require.Equal(t, parser.GeneratorOperator_GreaterThanEquals, ast.SetGenerator.GeneratorOperator)
 }
