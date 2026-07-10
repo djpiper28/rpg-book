b5376f96a436d708f82b76f66e551e0027670065
Danny Piper <djpiper28@gmail.com>
2026-07-10 11:24:51 +0100

    fix: electron path changed for some bloody reason

--- desktop_client/app/nix-bins.sh
+++ desktop_client/app/nix-bins.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pushd ./node_modules/.pnpm/electron@*/node_modules/@electron/get/dist/esm
+pushd ./node_modules/.pnpm/electron@*/node_modules/electron
 rm -f electron
 ln -s "$(whereis electron | cut -f 2 -d ' ')"
 popd
